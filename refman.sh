ac550a05a3b4d9e3c44684d659ab5f4ee99aec80
Soeren Gust <sgust@ithh.informationstheater.de>
2018-05-31 20:22:02 +0200

    Helper script to look at reference manual removed.

--- refman.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#
-xpdf -geometry -0+0 /archiv/bastelei/arm-st/Reference_Manual_RM0008_STM32F101xx_STM32F102xx_STM32F103xx_STM32F105xx_STM32F107xx_Rev16_CD00171190.pdf </dev/null >/dev/null 2>/dev/null &
